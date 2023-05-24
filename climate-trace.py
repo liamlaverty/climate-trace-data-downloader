e1d236cf1f5aa25a5794e573f5ce5dba49b5bdb5
Liam Laverty <liamlaverty@gmail.com>
2023-05-24 14:03:51 +0100

    downloads both forest and non-forest sectors

--- climate-trace.py
+++ climate-trace.py
@@ -1,56 +1,47 @@
 import os
 import requests
 import datetime
+from datalists.country_lists import countrylists
+from datalists.climate_trace_data_lists import climatetracedatalists
 
-climate_trace_files =[
-    { 
-        'url': 'sector_packages/buildings.zip',
-        'destPath': 'sector_packages',
-        'destName': 'buildings.zip'
-    },
-    { 
-        'url': 'sector_packages/fossil_fuel_operations.zip',
-        'destPath': 'sector_packages',
-        'destName': 'fossil_fuel_operations.zip'
-    },
-    { 
-        'url': 'sector_packages/manufacturing.zip',
-        'destPath': 'sector_packages',
-        'destName': 'manufacturing.zip'
-    },
-    { 
-        'url': 'sector_packages/power.zip',
-        'destPath': 'sector_packages',
-        'destName': 'power.zip'
-    },
-    { 
-        'url': 'sector_packages/waste.zip',
-        'destPath': 'sector_packages',
-        'destName': 'waste.zip'
-    },
-    { 
-        'url': 'sector_packages/agriculture.zip',
-        'destPath': 'sector_packages',
-        'destName': 'agriculture.zip'
-    },
-    { 
-        'url': 'sector_packages/transportation.zip',
-        'destPath': 'sector_packages',
-        'destName': 'transportation.zip'
-    },
-    { 
-        'url': 'sector_packages/forestry_and_land_use.zip',
-        'destPath': 'sector_packages',
-        'destName': 'forestry_and_land_use.zip'
-    },
-]
-
-
-def Main():
+def Do_Main():    
     t = datetime.datetime.now()
     file_name_date_part = t.strftime('%Y%m%d')
-    for i, file in enumerate(climate_trace_files):
-        download_file(f'https://downloads.climatetrace.org/{file["url"]}', file['destPath'], f'{file_name_date_part}_{file["destName"]}')
+    download_country_level_data(file_name_date_part)
+    download_sector_level_data(file_name_date_part)
+
+
+def download_country_level_data(file_path_date: str, download_forest_sectors:bool = True):
+    """
+    Downloads country level data from climate trace
+    """
+    
+    for i, country in enumerate(countrylists.country_three_char_list):
+        # download file
+        # https://downloads.climatetrace.org/country_packages/non_forest_sectors/AFG.zip
+        # https://downloads.climatetrace.org/country_packages/forest/AFG.zip
+
+        # download the non-forest-sectors data first, then the forest data sectors
+        file_url = f'https://downloads.climatetrace.org/country_packages/non_forest_sectors/{country["alpha-3"]}.zip'
+        dest_path = os.path.join('data_packages', 'climate_trace', 'country_packages', 'non_forest_sectors')
+        download_file(file_url, dest_path, f'{file_path_date}_{country["alpha-3"]}.zip')
+        
+        if download_forest_sectors:
+            # download the forest-sectors data first, then the forest data sectors
+            file_url = f'https://downloads.climatetrace.org/country_packages/forest/{country["alpha-3"]}.zip'
+            dest_path = os.path.join('data_packages', 'climate_trace', 'country_packages', 'forest')
+            download_file(file_url, dest_path, f'{file_path_date}_{country["alpha-3"]}.zip')
+        
+
+
+def download_sector_level_data(file_path_date: str):
+    """
+    Downloads sector level data from climate trace
+    """
+     
+    for i, file in enumerate(climatetracedatalists.climate_trace_files_list):
+        dest_path = os.path.join('data_packages', 'climate_trace', file['destPath'])
+        download_file(f'https://downloads.climatetrace.org/{file["url"]}', dest_path, f'{file_path_date}_{file["destName"]}')
 
 
 def download_file(url: str, destFilePath: str, destFileName:str):
@@ -76,5 +67,4 @@ def download_file(url: str, destFilePath: str, destFileName:str):
 
 
 
-
-Main()
+Do_Main()
